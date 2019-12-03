484c29b1e666f18bb6bc2669ed3647d36837c16c
Hongzhuo Liang <lianghongzhuo@126.com>
2019-12-03 22:47:17 +0100

    publish camera info

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@
 #include "std_msgs/String.h"
 
 #include <sstream>
+#include <tf/transform_broadcaster.h>
 
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
@@ -27,17 +29,17 @@ int main(int argc, char *argv[])
     nh.getParam("save_file", save_file);
 
     if (!camera.setIp(camera_ip)) return -1;
-	std::cout << "Camera IP: " << camera.getCameraIp() << std::endl
-		<< "Camera ID: " << camera.getCameraId() << std::endl
-		<< "Version: " << camera.getCameraVersion() << std::endl;
+    std::cout << "Camera IP: " << camera.getCameraIp() << std::endl
+    << "Camera ID: " << camera.getCameraId() << std::endl
+    << "Version: " << camera.getCameraVersion() << std::endl;
     CameraIntri intri = camera.getCameraIntri();
 	
-    ros::Publisher pub_color = nh.advertise<sensor_msgs::Image>("/color_image", 1);
-    ros::Publisher pub_depth = nh.advertise<sensor_msgs::Image>("/depth_image", 1);
-    ros::Publisher pub_pcl = nh.advertise<sensor_msgs::PointCloud2> ("/point_cloud", 1);
-    ros::Publisher pub_pcl_color = nh.advertise<sensor_msgs::PointCloud2> ("/color_point_cloud", 1);
-
-    ros::Rate loop_rate(5);
+    ros::Publisher pub_color = nh.advertise<sensor_msgs::Image>("/mechmind/color_image", 1);
+    ros::Publisher pub_depth = nh.advertise<sensor_msgs::Image>("/mechmind/depth_image", 1);
+    ros::Publisher pub_pcl = nh.advertise<sensor_msgs::PointCloud2>("/mechmind/point_cloud", 1);
+    ros::Publisher pub_pcl_color = nh.advertise<sensor_msgs::PointCloud2>("/mechmind/color_point_cloud", 1);
+    ros::Publisher pub_camera_info = nh.advertise<sensor_msgs::CameraInfo>("/mechmind/camera_info", 1);
+    ros::Rate loop_rate(1);
 
     while (nh.ok()) 
     {
@@ -58,6 +60,34 @@ int main(int argc, char *argv[])
     sensor_msgs::PointCloud2 ros_cloud;
     sensor_msgs::PointCloud2 ros_color_cloud;
 
+    // get camera info
+    sensor_msgs::CameraInfo camera_info;
+    camera_info.header.stamp = ros::Time::now();
+    camera_info.header.frame_id = "mechmind_camera";
+    camera_info.height = depth.rows;
+    camera_info.width = depth.cols;
+    camera_info.distortion_model = "plumb_bob";
+    std::vector<double> D(5, 0.0);
+    camera_info.D = D;
+
+    std::vector<double> K{intri.fx, 0.0, intri.u, 0.0, intri.fy, intri.v, 0.0, 0.0, 1.0};
+    for(size_t i = 0; i < 9; ++i)
+    {
+      camera_info.K[i] = K[i];
+    }
+
+    std::vector<double> R{1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
+    for(size_t i = 0; i < 9; ++i)
+    {
+      camera_info.R[i] = R[i];
+    }
+
+    std::vector<double> P{intri.fx, 0.0, intri.u, 0.0, 0.0, intri.fy, intri.v, 0.0, 0.0, 0.0, 1.0, 0.0};
+    for(size_t i = 0; i < 12; ++i)
+    {
+      camera_info.P[i] = P[i];
+    }
+
     cv_image.toImageMsg(ros_image);
     cv_depth.toImageMsg(ros_depth);
     pcl::toROSMsg(cloud, ros_cloud);
@@ -66,6 +96,7 @@ int main(int argc, char *argv[])
     color_cloud = PointCloudTools::getColoredCloud(color, depth, intri);
     pcl::toROSMsg(color_cloud, ros_color_cloud);
     ros_image.header.frame_id = "mechmind_camera";
+    ros_image.header.stamp = camera_info.header.stamp;
     ros_depth.header.frame_id = "mechmind_camera";
     ros_cloud.header.frame_id = "mechmind_camera";
     ros_color_cloud.header.frame_id = "mechmind_camera";
@@ -74,6 +105,16 @@ int main(int argc, char *argv[])
     pub_color.publish(ros_image);
     pub_depth.publish(ros_depth);
     pub_pcl_color.publish(ros_color_cloud);
+    pub_camera_info.publish(camera_info);
+
+    tf::TransformBroadcaster broadcaster;
+    tf::StampedTransform stColorOpt;
+    tf::Quaternion qZero;
+    qZero.setRPY(0, 0, 0);
+    tf::Vector3 vZero(0, 0, 0);
+    tf::Transform tZero(qZero, vZero);
+    stColorOpt = tf::StampedTransform(tZero, ros::Time::now(), "mechmind_camera", "mechmind_camera");
+    broadcaster.sendTransform(stColorOpt);
 
     if (save_file){
         cv::imwrite("/tmp/mechmind_depth.png", depth);
